f39e4151140bfdca2d7546cfa9e46434ff8ea2ae
Alex <alex@ruddick.tech>
2023-12-03 19:00:44 -0600

    Day 2, part 2

--- src/day2/mod.rs
+++ src/day2/mod.rs
@@ -1,29 +1,25 @@
 #[path = "../util/util.rs"] mod util;
 
-#[derive(Debug)] struct CubeDraw(i8, i8, i8);
+#[derive(Debug)] struct CubeDraw(i32, i32, i32);
 
 pub fn main() {
     println!("Day 2: Cube Conundrum");
-    const MAX_RED: i8 = 12;
-    const MAX_GREEN: i8 = 13;
-    const MAX_BLUE: i8 = 14;
-    let mut total: i32 = 0;
     let mut games = Vec::new();
-    'outer: for line in util::read_input("src/day2/input.txt") {
+    for line in util::read_input("src/day2/input.txt") {
         let colon = line.find(":").unwrap();
         let gameno = &line[5..colon]
                            .parse::<i32>().unwrap();
-        println!("Game {gameno}:");
-        games.push((*gameno, Vec::<CubeDraw>::new()));
+        // println!("Game {gameno}:");
         let gameline = &line[colon+2..];
-        for draws in gameline.split(";") {
+        let mut draws = Vec::<CubeDraw>::new();
+        for drawlines in gameline.split(";") {
             let mut red = 0;
             let mut green = 0;
             let mut blue = 0;
-            for mut draw in draws.split(",") {
+            for mut draw in drawlines.split(",") {
                 draw = draw.trim();
                 let space = draw.find(" ").unwrap();
-                let num = draw[0..space].parse::<i8>().unwrap();
+                let num = draw[0..space].parse::<i32>().unwrap();
                 let color = draw.as_bytes()[space + 1] as char;
                 match color {
                     'r' => red = num,
@@ -33,16 +29,44 @@ pub fn main() {
                 }
             }
             let set = CubeDraw(red, green, blue);
-            if red > MAX_RED || blue > MAX_BLUE || green > MAX_GREEN {
-                println!("Impossible\n");
-                continue 'outer;
-            }
-            print!("{set:?}");
-            // println!("r:{red}, g:{green}, b:{blue}");
-            print!("\n");
+            draws.push(set);
         }
-        total += gameno;
+        games.push((*gameno, draws));
 
     }
-    println!("The sum of the total cube IDs is {total}.");
+    // part 1
+    const MAX_RED: i32 = 12;
+    const MAX_GREEN: i32 = 13;
+    const MAX_BLUE: i32 = 14;
+    let mut total: i32 = 0;
+    'outer: for game in &games {
+        let gameno = game.0;
+        // println!("Game {gameno}:");
+        for draw in &game.1 {
+            // println!("{draw:?}");
+            if draw.0 > MAX_RED || draw.1 > MAX_GREEN || draw.2 > MAX_BLUE {
+                // println!("impossible");
+                continue 'outer
+            }
+        }
+        total += gameno;
+    }
+    println!("The sum of the possible cube IDs is {total}.");
+
+    // part 2
+    total = 0;
+    for game in games {
+        let mut min_red: i32 = 0;
+        let mut min_green: i32 = 0;
+        let mut min_blue: i32 = 0;
+        for draw in game.1 {
+            if draw.0 > min_red { min_red = draw.0}
+            if draw.1 > min_green { min_green = draw.1}
+            if draw.2 > min_blue { min_blue = draw.2}
+        }
+        let power:i32 = min_red * min_blue * min_green;
+        total += power;
+    }
+    println!("The total cube power is {total}.   That's over 9000!");
+
 }
