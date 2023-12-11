49a7d61b66cf2adb874c2216e041c981d6bc318d
Alex Ruddick <alex@ruddick.tech>
2023-12-11 11:54:29 -0600

    Day 11, part 2

--- src/day11/mod.rs
+++ src/day11/mod.rs
@@ -8,9 +8,23 @@ pub fn main() {
         let v: Vec<char> = line.split_whitespace().flat_map(|i| i.chars()).collect();
         input_image.push(v);
     }
-    let expanded_rows_image = expand_empty_rows(&input_image);
-    let expanded_cols_image = expand_empty_rows(&transpose(&expanded_rows_image));
-    let image = transpose(&expanded_cols_image);
+    // let expanded_rows_image = expand_empty_rows(&input_image);
+    // let expanded_cols_image = expand_empty_rows(&transpose(&expanded_rows_image));
+    // let image = transpose(&expanded_cols_image);
+    let image = input_image;
+
+    let mut expanded_rows = Vec::new();
+    for (i, row) in image.iter().enumerate() {
+        if !row.contains(&'#') {
+            expanded_rows.push(i);
+        }
+    }
+    let mut expanded_cols = Vec::new();
+    for (i, row) in transpose(&image).iter().enumerate() {
+        if !row.contains(&'#') {
+            expanded_cols.push(i);
+        }
+    }
 
     let rows = image.len();
     let cols = image[0].len();
@@ -19,22 +33,40 @@ pub fn main() {
         for col in 0..cols {
             let c = image[row][col];
             if c == '#' {
-                galaxies.push((row as i32, col as i32));
+                galaxies.push((row as i64, col as i64));
             }
         }
     }
 
     let mut galaxy_pairs = Vec::new();
-    let mut total = 0;
+    let mut total1 = 0;
+    let mut total2 = 0;
     for (i, &x) in galaxies.iter().enumerate() {
         for &y in galaxies.iter().skip(i + 1) {
             galaxy_pairs.push((x, y));
-            total += num::abs(x.0 - y.0);
-            total += num::abs(x.1 - y.1);
+            total1 += num::abs(x.0 - y.0);
+            total2 += num::abs(x.0 - y.0);
+            for i in &expanded_rows {
+                if (*i as i64) > std::cmp::min(x.0, y.0) && (*i as i64) < std::cmp::max(x.0, y.0) {
+                    // println!("{x:?} {i} {y:?}");
+                    total1 += 2 - 1;
+                    total2 += 1000000 - 1;
+                }
+            }
+            for i in &expanded_cols {
+                if (*i as i64) > std::cmp::min(x.1, y.1) && (*i as i64) < std::cmp::max(x.1, y.1) {
+                    // println!("{x:?} {i} {y:?}");
+                    total1 += 2 - 1;
+                    total2 += 1000000 - 1;
+                }
+            }
+            total1 += num::abs(x.1 - y.1);
+            total2 += num::abs(x.1 - y.1);
         }
     }
 
-    println!("The distance between all the galaxies is {total}.");
+    println!("The distance between all the galaxies is {total1}.");
+    println!("The distance between all the galaxies is {total2}.");
 
 }
 
