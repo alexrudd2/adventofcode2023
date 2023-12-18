25aef7a73deee019369f95fd3c2e29301c477f64
Alex Ruddick <alex@ruddick.tech>
2023-12-17 21:28:31 -0600

    improve day selector scripts

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,62 @@
-// mod day1;
-// mod day2;
-// mod day3;
-// mod day4;
-// mod day5;
-// mod day6;
-// mod day7;
-// mod day8;
-// mod day9;
-// mod day10;
-// mod day11;
-// mod day12;
-// mod day13;
+mod day1;
+mod day2;
+mod day3;
+mod day4;
+mod day5;
+mod day6;
+mod day7;
+mod day8;
+mod day9;
+mod day10;
+mod day11;
+mod day12;
+mod day13;
 mod day17;
+use std::env;
+use std::process::Command;
 
 fn main() {
-    // day1::main();
-    // day2::main();
-    // day3::main();
-    // day4::main();
-    // day5::main();
-    // day6::main();
-    // day7::main();
-    // day8::main();
-    // day9::main();
-    // day10::main();
-    // day11::main();
-    // day12::main();
-    // day13::main();
-    // day14::main();
-    // day15::main();
-    // day16::main();
-    day17::main();
-    // day18::main();
-    // day19::main();
-    // day20::main();
-    // day21::main();
-    // day22::main();
-    // day23::main();
-    // day24::main();
+    let args: Vec<String> = env::args().collect();
+    let mut day = 0;
+
+    if args.len() < 2 {
+        println!("Choose a day");
+    } else {
+        // Access the first argument provided
+        let argument = &args[1];
+        match argument.parse::<i32>() {
+            Err(_) => println!("Failed to parse the argument as an integer."),
+            Ok(value) => day = value,
+        }
+    }
+    let _ = Command::new("clear").status();
+    match day {
+        1 => day1::main(),
+        2 => day2::main(),
+        3 => day3::main(),
+        4 => day4::main(),
+        5 => day5::main(),
+        6 => day6::main(),
+        7 => day7::main(),
+        8 => day8::main(),
+        9 => day9::main(),
+        10 => day10::main(),
+        11 => day11::main(),
+        12 => day12::main(),
+        13 => day13::main(),
+        // 14 => day14::main(),
+        // 15 => day15::main(),
+        // 16 => day16::main(),
+        17 => day17::main(),
+        // 18 => day18::main(),
+        // 19 => day19::main(),
+        // 20 => day20::main(),
+        // 21 => day21::main(),
+        // 22 => day22::main(),
+        // 23 => day23::main(),
+        // 24 => day24::main(),
+        // 25 => day25::main(),
+        _ => println!("WTF is this, January?"),
+    }
+    
 }
--- watch.sh
+++ watch.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-FILE_TO_WATCH="./src/day17/mod.rs"
-COMMAND_TO_RUN="cargo run"
+# Check if an argument is provided
+if [ "$#" -ne 1 ]; then
+    echo "Enter a day!"
+    exit 1
+fi
+
+day="$1"
+
+FILE_TO_WATCH="./src/day${day}/mod.rs"
+COMMAND_TO_RUN="cargo run ${day}"
 
 while true; do
-    inotifywait -e modify "$FILE_TO_WATCH" && clear && $COMMAND_TO_RUN
+    inotifywait -e modify "$FILE_TO_WATCH" && $COMMAND_TO_RUN
 done
